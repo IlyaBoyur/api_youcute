28cf370407eb5125c638aad9553f96b25606b11b
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-05-19 12:45:52 +0300

    Add Follow and Group models and admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Follow, Post, Group
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'post', 'author', 'text', 'created')
+    search_fields = ('text',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Comment, CommentAdmin)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("pk", "user", "author")
+
+
+admin.site.register(Follow, FollowAdmin)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug", "description")
+    search_fields = ("title",)
+    empty_value_display = "-пусто-"
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Group, GroupAdmin)
--- api/models.py
+++ api/models.py
@@ -1,16 +1,20 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+
 User = get_user_model()
 
 
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(
-        "Дата публикации", auto_now_add=True
+        "Дата публикации",
+        auto_now_add=True,
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="posts"
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
     )
 
     def __str__(self):
@@ -19,12 +23,46 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="comments"
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
     )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
     )
     text = models.TextField()
     created = models.DateTimeField(
-        "Дата добавления", auto_now_add=True, db_index=True
+        "Дата добавления",
+        auto_now_add=True,
+        db_index=True,
+    )
+
+
+class Group(models.Model):
+    title = models.CharField(
+        "Заголовок",
+        max_length=200,
+        help_text="Дайте название группе"
+    )
+    slug = models.SlugField(
+        unique=True,
+    )
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="follower",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="following",
     )
