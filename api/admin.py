9a6d64dd7f7a1bf17efc8bcc7898a9b6046c9ffe
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-05-19 14:17:21 +0300

    Update admin to include group

--- api/admin.py
+++ api/admin.py
@@ -4,7 +4,7 @@ from .models import Comment, Follow, Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date', 'author')
+    list_display = ('pk', 'text', 'pub_date', 'author', "group")
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
