baafda67a50d3daabe5cc1739a9333d169c70e9a
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-05-19 14:20:48 +0300

    Add posts API

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,15 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, PostViewSet
 
 
+router_v1 = DefaultRouter()
+router_v1.register('v1/posts',
+                   PostViewSet,
+                   basename='posts')
 urlpatterns = [
     path('v1/token/',
          TokenObtainPairView.as_view(),
@@ -12,4 +19,5 @@ urlpatterns = [
     path('v1/token/refresh/',
          TokenRefreshView.as_view(),
          name='token_refresh'),
+    path('', include(router_v1.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,1 +1,22 @@
+import django_filters.rest_framework
+from rest_framework import permissions, viewsets
+from rest_framework.generics import get_object_or_404
+
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import CommentSerializer, PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsAuthorOrReadOnly,)
+    filter_backends=[django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields=['group',]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
 # TODO:  Напишите свой вариант
