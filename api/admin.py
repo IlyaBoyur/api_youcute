63bad59dee469ea51f93d229474abbd10f7aa3e9
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-05-21 07:44:56 +0300

    Optimize admin cfg via decorators vs explicit registration

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Comment, Follow, Group, Post
 
-
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author', "group")
     search_fields = ('text',)
@@ -10,29 +10,20 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-admin.site.register(Post, PostAdmin)
-
-
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('pk', 'post', 'author', 'text', 'created')
     search_fields = ('text',)
     empty_value_display = '-пусто-'
 
 
-admin.site.register(Comment, CommentAdmin)
-
-
+@admin.register(Follow)
 class FollowAdmin(admin.ModelAdmin):
     list_display = ("pk", "user", "following")
 
 
-admin.site.register(Follow, FollowAdmin)
-
-
+@admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("pk", "title")
     search_fields = ("title",)
     empty_value_display = "-пусто-"
-
-
-admin.site.register(Group, GroupAdmin)
