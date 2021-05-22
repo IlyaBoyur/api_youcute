b9381c3793dcc3aa01976b1f242a5251116dc721
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-05-22 09:44:11 +0300

    Change serializers fields to __all__ for better changebility

--- api/serializers.py
+++ api/serializers.py
@@ -7,23 +7,23 @@ class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
-        fields = ('id', 'text', 'author', 'pub_date')
         model = Post
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
-        fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
+        fields = '__all__'
         read_only_fields = ('post',)
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('title',)
         model = Group
+        fields = '__all__'
 
 
 class FollowSerializer(serializers.ModelSerializer):
@@ -31,5 +31,5 @@ class FollowSerializer(serializers.ModelSerializer):
     following = serializers.CharField(source='following.username')
 
     class Meta:
-        fields = ('user', 'following')
         model = Follow
+        fields = ('user', 'following')
